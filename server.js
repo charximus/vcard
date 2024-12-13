be262f0751808244cc9cb28697cb7bf09a810db4
charximus <caguilar@go-global.Us>
2024-12-13 15:21:10 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,19 @@ app.get('/download', async (req, res) => {
   const fileUrl = req.query.url;
 
   try {
+    // Descargar el archivo remoto desde la URL proporcionada
     const response = await axios.get(fileUrl, { responseType: 'arraybuffer' });
+
+    // Configurar los encabezados para servir el archivo como descarga
     res.set({
-      'Content-Type': 'text/vcard',
-      'Content-Disposition': 'attachment; filename=\"contact.vcf\"',
+      'Content-Type': 'text/vcard', // Tipo MIME correcto
+      'Content-Disposition': 'attachment; filename="contact.vcf"', // Forzar descarga
     });
+
+    // Enviar el archivo al cliente
     res.send(response.data);
   } catch (error) {
-    console.error(error);
+    console.error(`Error fetching the file: ${error.message}`);
     res.status(500).send('Error fetching the file');
   }
 });
